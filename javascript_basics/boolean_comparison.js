a6d0a47fa184919c9d79871a4e0966a7d6b13b23
xShame <kevin.camu07@gmail.com>
2021-07-01 21:20:01 +0800

    javascript_basics Done Boolean Comparison

---
+++ javascript_basics/boolean_comparison.js
@@ -0,0 +1,30 @@
+// boolean & comparisons
+console.log(true,false, "true","false");
+
+// methods can rreturn booleans
+let email = 'xShame@myemail.com';
+let names = ['mario', 'luigi','toad'];
+
+// let result = email.includes('@');
+// let result = names.includes('luigi');
+
+// console.log(result);
+
+// comparison operators
+
+let age = 25;
+
+// console.log(age == 25);
+// console.log(age == 30);
+// console.log(age !== 30);
+// console.log(age > 20);
+// console.log(age <= 25);
+// console.log(age >= 25);
+
+let name = 'xShame';
+
+console.log(name == 'xShame');
+console.log(name == 'Xshame');
+console.log(name > 'crystal');
+console.log(name > 'XShame');
+console.log(name > 'Crystal');
