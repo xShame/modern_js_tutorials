1a411f635895b06312c283d883037926ccb0fabf
xShame <kevin.camu07@gmail.com>
2021-07-01 20:52:10 +0800

    javascript_basics Done in Array

---
+++ javascript_basics/arrays.js
@@ -0,0 +1,24 @@
+let ninjas = ['mommy','kanna','seijuro','daddy'];
+// let ninjas = []; //empty array
+// ninjas.push('mommy','kanna','seijuro','daddy'); //to add array element to an empty array
+// ninjas[1] = 'kanna krystal';
+// console.log(ninjas);
+
+// let ages = [20,25,30,35];
+// console.log(ages[2])
+
+// let random = ['xShame', 'BSOD',30,20];
+// console.log(random);
+
+// console.log(ninjas.length);
+
+// array methods
+
+// let result = ninjas.join(','); 
+// let result = ninjas.indexOf('seijuro');
+// let ninjasEnemy = ['Dark Lord','Baphomet']
+// let result = ninjas.concat(ninjasEnemy);
+let result = ninjas.push('xShame');
+result = ninjas.pop() //to remove last item in array list
+
+console.log(ninjas);
