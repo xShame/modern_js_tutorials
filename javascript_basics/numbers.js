e56a907ec2cfc8ea4b6a931de1fca36642c67530
xShame <kevin.camu07@gmail.com>
2021-07-01 20:13:31 +0800

    javascript_basics Done in Numbers

---
+++ javascript_basics/numbers.js
@@ -0,0 +1,39 @@
+let radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi);
+
+// math operators +, -, *, /, **, %
+
+// console.log(10 / 2);
+// let result = radius % 3; //get the remainder
+
+// let result = pi * radius **2;
+
+// orrder of operation - B I D M A S
+
+// let result = 5 * (10-3)**2;
+
+// console.log(result);
+
+let likes = 10;
+
+// likes = likes + 1;
+// likes++;
+// likes--;
+
+// likes += likes;
+// likes -= likes;
+// likes *= likes;
+// likes /= likes;
+
+// console.log(likes);
+
+// NaN - Not a Numbers
+
+// console.log(5 / 'hello');
+// console.log(5 * 'hello');
+
+let result = 'the blog hass ' + likes + ' likes';
+
+console.log(result);
