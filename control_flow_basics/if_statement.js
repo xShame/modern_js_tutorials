ca72db9e3ed06e8760cab80cad0205be1dd2fbea
xShame <kevin.camu07@gmail.com>
2021-07-06 23:26:55 +0800

    control_flow_basics Done in If Statement

---
+++ control_flow_basics/if_statement.js
@@ -0,0 +1,18 @@
+// if statements
+// const age = 25;
+
+// if(age > 20){
+// 	console.log('you arre over 20 years old');
+// }
+
+// const ninjas =['shaun','ryu','chun-li','yoshi'];
+
+// if (ninjas.length > 3) {
+// 	console.log("that's alot of ninjas");
+// }
+
+const password = 'pass';
+
+if (password.length >= 8) {
+	console.log('that password is long enough!');
+}
