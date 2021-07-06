1abdd76d4a1d9ca09799bc1a6af1547db5f440ea
xShame <kevin.camu07@gmail.com>
2021-07-06 23:15:50 +0800

    control_flow_basics Done in While Loop

---
+++ control_flow_basics/while_loops.js
@@ -0,0 +1,13 @@
+// while loops
+// let i = 0;
+// while(i < 5){
+// 	console.log(`in loop: ${i}`);
+// 	i++;
+// }
+
+const names = ['mommy','kanna','seijuro','daddy'];
+let i = 0;
+while (i < names.length){
+	console.log(names[i]);
+	i++;
+}
