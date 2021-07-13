ae482cfdea488cd3e3346d698d03314edf276691
xShame <kevin.camu07@gmail.com>
2021-07-13 23:22:03 +0800

    functions_methods Done in Function Declarations and Expressions

---
+++ functions_methods/functions-declarations_expressions.js
@@ -0,0 +1,14 @@
+// function declaration
+
+function greetings(){
+	console.log("Hello World!, my First Function creation...");
+}
+
+greetings();
+
+// function expression
+const speak = function(){
+	console.log('function expression');
+};
+
+speak();
