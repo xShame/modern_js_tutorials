67dc3384c922b5557956293d90b0355aa30dbbc3
xShame <kevin.camu07@gmail.com>
2021-07-27 21:44:06 +0800

    functions_methods Done in Foreach Methods and Callbacks

---
+++ functions_methods/foreach-method_callbacks.js
@@ -0,0 +1,19 @@
+// callback & foreach
+
+// const myFunc = (callbackFunc) =>{
+// 	// do something
+// 	let value = 50;
+// 	callbackFunc(value);
+// }
+
+// myFunc(value =>{
+// 	// do something
+// 	console.log(value);
+// });
+
+let people = ['mario','luigi','ryu','shaun','chun-li'];
+
+const logPerson = (person,index) =>{
+	console.log(`${index} - hello ${person}`)
+}
+people.forEach(logPerson);
