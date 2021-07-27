5cb4a17770fd4dddbcb48804bf2d34ce9d7235aa
xShame <kevin.camu07@gmail.com>
2021-07-27 22:11:15 +0800

    functions_methods Done in Callback Function

---
+++ functions_methods/callback-functions_action.js
@@ -0,0 +1,14 @@
+// get reference from the ul
+
+let ul = document.querySelector('.people');
+
+const people = ['mario','luigi','ryu','shaun','chun-li'];
+
+let html = ``;
+people.forEach(function(person){
+	// create HTML template
+	html += `<li style:"color: purple">${person}</li>`;
+});
+
+console.log(html);
+ul.innerHTML = html;
