737754e9598145df01a3b39ddb25492e99a7cbad
Kevin Eusebio <kceusebio.enclvewrx@joy-nostalg.com>
2023-11-26 01:59:59 +0800

    Babel and Webpack use together

--- babel_webpack/webpack.config.js
+++ babel_webpack/webpack.config.js
@@ -11,4 +11,18 @@ module.exports = {
       directory: path.resolve(__dirname, "dist"),
     },
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
